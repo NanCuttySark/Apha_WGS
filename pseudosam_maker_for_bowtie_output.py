17c858437be4694a312d1f4de99789c52498826a
NanCuttySark <526473+gb96@users.noreply.github.com>
2019-02-04 11:41:52 +0300

    Script for making of pseudosam from output of bowtie2

--- pseudosam_maker_for_bowtie_output.py
+++ pseudosam_maker_for_bowtie_output.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 import os
-import re
+
 
 # paths for IITP cluster:
 # pri_path = '/home/ishtar/Glycines'
 # sec_path = '_Gly/vs_all_bact_'
 
 # path for Roma's cluster:
-
 pri_path = '~/Yulya/Glycines'
 sec_path = '_Gly/vs_all_bact_'
-
 
 for i in range(1, 21):
     input_file_name = str('Gly_' + str(i) + '_vs_all_bact_global_k5_bowtie.sam')
     output_file_name = str('shorten_Gly_' + str(i) + '_vs_all_bact_k5_bowtie.pseudosam')
     outFile = open(output_file_name, "w")
+
+    full_path = os.path.join(pri_path, str(i) + sec_path + str(i) + '_Gly')
+    input_file_path = os.path.join(full_path, input_file_name)
+    output_file_path = os.path.join(full_path, output_file_name)
 
     inFile = open(input_file_name, "r")
     # print(fileName)
@@ -29,5 +31,5 @@ for i in range(1, 21):
             outFile.write(line)
     outFile.close()
 
-    #outFile=open("apha_extract_unique_alignments.pseudosam","w")
-    #outFile.close()
+    # outFile=open("apha_extract_unique_alignments.pseudosam","w")
+    # outFile.close()
